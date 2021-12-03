14fc52161a26f2ba37aff62bdbc980b46018ae03
Sebastian Abril <sebastianabril6@gmail.com>
2021-12-02 20:22:23 -0500

    Ejercicio #3 y #4

---
+++ MakeItReal/Intro-Javascript/Ejercicio3.js
@@ -0,0 +1,5 @@
+let numero1 = parseFloat(prompt("Escribe tu primero numero"));
+let numero2 = parseFloat(prompt("Escribe tu segundo numero"));
+
+let resultado = numero1+numero2;
+console.log("La suma de los numeros es "+resultado);
---
+++ MakeItReal/Intro-Javascript/Ejercicio4.js
@@ -0,0 +1,5 @@
+let birthYear = prompt("Escribe tu año de nacimiento");
+let currentYear = 2021;
+let age = currentYear-birthYear;
+
+console.log("Tienes "+age+" años");
