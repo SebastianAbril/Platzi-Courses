358bd28a27ff9c22f0c04670acdfa3296d6a97d5
Sebastian Abril <sebastianabril6@gmail.com>
2021-12-27 15:43:52 -0500

    que es NPM? node package manager

---
+++ Platzi/NPM/clase1.js
@@ -0,0 +1,6 @@
+/*
+¿Qué es NPM (node package manager) ? Es un gestor de paquetes, el más popular que tiene JavaScript, 
+donde encontrarás una gran cantidad de recursos para poder implementar en tus proyectos. También vas a 
+poder crear tus propios paquetes y compartirlos con toda la comunidad.
+https://www.npmjs.com/
+*/
