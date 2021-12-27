1d53d78a6ae6e34e3a376e87aa363c269e0cde23
Sebastian Abril <sebastianabril6@gmail.com>
2021-12-27 16:00:28 -0500

    Clase 2 y 3

--- Platzi/NPM/clase1.js
+++ Platzi/NPM/clase1.js
@@ -1,6 +1,20 @@
-/*
+/* Clase1.
 ¿Qué es NPM (node package manager) ? Es un gestor de paquetes, el más popular que tiene JavaScript, 
 donde encontrarás una gran cantidad de recursos para poder implementar en tus proyectos. También vas a 
 poder crear tus propios paquetes y compartirlos con toda la comunidad.
 https://www.npmjs.com/
+
+Clase2.
+Se instala la ultima la version de Node.js
+Para verificar que quedo instalado, se abre la consola de windows
+y luego:
+
+node -v (mirar la version de node.js)
+npm - v(mirar la version de npm)
+
+npm install -g npm@latest (instalar la ultima version de npm)
+
+Clase3.
+Instalacion en Mac
+
 */
