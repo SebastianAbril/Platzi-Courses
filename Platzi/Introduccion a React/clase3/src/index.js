c080920807d0925ceff56b88519fca19047e2244
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-03-08 13:52:02 -0500

    clase #5 Platzi

--- Platzi/Introduccion a React/clase3/src/index.js
+++ Platzi/Introduccion a React/clase3/src/index.js
@@ -2,25 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import reactDom from 'react-dom';
-
-// APUNTES CLASE PLATZI
-const e = React.createElement;
-
-function App() {
-  return (
-    React.createElement('h1', {id: 'title'}, 'Oli React')
-  );
-  }
-
-
-
-reactDom.render(e());
-
-
-
-
 
 
 ReactDOM.render(
@@ -30,7 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
