bac9e0c6edaa49b87c8aabd267adcf1da7037630
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-06-18 14:01:18 -0500

    clase # 9: Variables y clase #10: tipos de dato

---
+++ Platzi/BasicoPython/clase10.py
@@ -0,0 +1,15 @@
+from socket import J1939_MAX_UNICAST_ADDR
+
+
+nombre1 = "Juan"
+nombre2 = 'Sebastian'
+
+concatenacion = nombre1 + nombre2
+print(concatenacion)
+
+numero_decimal = 3.141516
+
+booleano = True
+
+es_estudiante = True
+trabaja = False
---
+++ Platzi/BasicoPython/clase9.py
@@ -0,0 +1,10 @@
+""" Una variable es una caja o lugar en donde puedo guardar objetos: números, texto, etc.and
+
+El identificador de mi variable no puede comenzar con un número 
+y debe estar en minusculas. Las palabras dentro del mismo se separan con guion bajo """
+
+
+numero1 = 11
+numero2 = 13
+numero_resultado = numero1 + numero2
+
