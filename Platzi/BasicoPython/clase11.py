5dab8772d1ba881e06781459f03f646ad928ca5d
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-06-18 19:05:51 -0500

    clase #11 cambiar de un tipo de Dato a otro

---
+++ Platzi/BasicoPython/clase11.py
@@ -0,0 +1,11 @@
+#Convetir a otro tipo de dato
+
+numero1 = input("Escribe un número: ")
+numero2 = input("Escriber un número: ")
+
+numero1 = int(numero1) 
+numero2 = int(numero2)
+
+numero_decimal = 4.5
+int(numero_decimal);
+str(numero_decimal)
