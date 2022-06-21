5bcb78652539b5bcf04783d07ab1cd9bc1b2ffb1
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-06-21 09:23:47 -0500

    Clase #15: elif, if, else: convesor de monedas argentina, colombiana y mexicana

--- Platzi/BasicoPython/conversor.py
+++ Platzi/BasicoPython/conversor.py
@@ -1,9 +1,41 @@
-pesos = input("¿Cuántos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " ")
+menu = """""
+Bienvenidos al conversor de monedas 💰
+
+1- Pesos colombianos
+2- Pesos argentinos
+3- Pesos mexicanos
+
+Elige una opción:"""
+
+opcion= int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " ")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " ")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " ")
+else:
+    print('Ingresa una opción correcta por favor')
+
+
 
 
