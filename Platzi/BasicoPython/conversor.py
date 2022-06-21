382c5bbd5f11b1e65df586b2dbaa7d6fdea38ffd
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-06-21 11:06:39 -0500

    Modularizando el conversor de monedas, uso de funcionas: DRY (Dont repeat yourself)

--- Platzi/BasicoPython/conversor.py
+++ Platzi/BasicoPython/conversor.py
@@ -1,3 +1,11 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuántos pesos "+ tipo_pesos +" tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " ")
+
 menu = """""
 Bienvenidos al conversor de monedas 💰
 
@@ -9,30 +17,13 @@ Elige una opción:"""
 
 opcion= int(input(menu))
 
+
 if opcion == 1:
-    pesos = input("¿Cuántos pesos colombianos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " ")
+    conversor("colombianos", 3875)
 elif opcion == 2:
-    pesos = input("¿Cuántos pesos argentinos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " ")
+    conversor("argentinos", 65)
 elif opcion == 3:
-    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " ")
+    conversor("mexicanos", 24)
 else:
     print('Ingresa una opción correcta por favor')
 
--- Platzi/BasicoPython/funciones.py
+++ Platzi/BasicoPython/funciones.py
@@ -14,7 +14,7 @@ def conversacion(mensaje):
     print('Adios')
 
 
-opcion = int(input('Elige una opcion(1, 2, 3): '))
+""" opcion = int(input('Elige una opcion(1, 2, 3): '))
 if opcion == 1:
     conversacion('Elegiste la opción 1')
 elif opcion == 2:
@@ -22,4 +22,12 @@ elif opcion == 2:
 elif opcion == 3:
     conversacion('Elegiste la opción 3')
 else:
-    print('Escribe la opción correcta')
+    print('Escribe la opción correcta') """
+
+def suma(a, b):
+    print('Se suman dos números')
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
