e5574887cfdb69d9e82b3ddf1d359cd26adfa3af
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-06-24 09:37:43 -0500

    Clase #6: multiples muchos

---
+++ Platzi/FundamentosBaseDatos/clase6.js
@@ -0,0 +1,3 @@
+/* 
+
+Cardinalidades múltiples muchos: También conocida como “Muchos a Muchos”. Es el tipo de cardinalidad en el que muchas entidades de un tipo, pertenecen a muchas entidades de otro, la cual debe ser normalizada y relacionada a partir de llaves foráneas. */
