13b4ecf58ff8f139f691f876febbdf985eedb660
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-06-28 15:35:12 -0500

    Clase #8: Tipos de datos y restricciones

---
+++ Platzi/FundamentosBaseDatos/clase8.js
@@ -0,0 +1,17 @@
+/**  TIPOS DE DATO
+ * Texto: CHAR(n), VARCHAR(n), TEXT
+ * Numeros: INTEGER, BIGINT, SMALLINT, DECIMAL(n,s), NUMERIC(n,s)
+ * Fecha/hora: DATE, TIME, DATETIME, TIMESTAMP
+ * Lógicos: BOOLEAN
+ * **/
+
+
+/*** CONSTRAINS (RESTRICCIONES) 
+ * NOT NULL , que no tenga valores nulos
+ * UNIQUE   , se asegura que cada valor en la columna no se repita
+ * PRIMARY KEY   , Es una combinacion de NOT NULL y UNIQUE
+ * FOREIGN KEY   , identifica de manera única una tupla en otra tabla
+ * CHECK  , Se asegura que el valor en la columna cumpla una condición dada
+ * DEFAULT Coloca un valor por defecto cuando no hay un valor especificado
+ * INDEX  , se crea por columna para permitir búsquedas más rápidas.
+ * ***/
