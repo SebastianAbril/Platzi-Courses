e5f18f4a34adb464530101357d355182c00bbcc5
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-07-15 09:36:57 -0500

    clase #4

---
+++ Platzi/EstrategiasAprenderEnLinea/clase4.js
@@ -0,0 +1,11 @@
+/**
+ * ¿Como retener más informacion?
+ *  poniendo atención, la atención tiene dos partes:
+ * - Selectividad.
+ * -Esfuerzo Mental.
+ * 
+ * La atención se puede dirigir (mientras más complejo el tema, más esfuerzo se requiere)
+Estudia en espacios callados y limpios
+No tengas tu celular u otros dispositivos que te puedan distraer al estudiar
+Toma notas al estudiar
+ */
