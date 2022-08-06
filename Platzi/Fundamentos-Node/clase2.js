7297edeed26685308e2d0173470aee32228690d6
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-08-05 22:03:49 -0500

    clase #2 introduccion a Node

---
+++ Platzi/Fundamentos-Node/clase2.js
@@ -0,0 +1,22 @@
+/**
+ * Node es un entorno de ejecucion de JS fuera del navegtador.
+ *  Se crea en 2009 y es orientado a servidores.
+ * - Servidores
+ * -Herramientas: transpiladores, scraping, automatizacion
+ * Bases:
+ * - Monohilo, con entradas y salidas asíncronas.
+ * - Un proceso por cada núcleo del procesador.
+ * 
+ * Node corre sobre el Motor V8: entorno de ejecucion de JS creado
+ * por Google, y libre desde 2008.
+ * Escrito en C++
+ * Convierte JS en código máquina en lugar de interpretarlo en tiempo de real.
+ * 
+ * Todo funciona con base a Modulos:
+ * Todo lo que no sea sintaxis de programacion (if, else,for)
+ * son módulos. Muchos módulos vienen por defecto en el paquete
+ * de Node. Puedes crear tus propios módulos.
+ * 
+ * Orientado a eventos: hay un bucle de eventos que se ejecuta constantemente.
+ * Puedes orientar tu código de forma reactiva.
+ */
