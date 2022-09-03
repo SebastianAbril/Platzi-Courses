0d800d6256307505cb4d2c6c25148dc701304d0c
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-03 10:44:27 -0500

    clase #9: variables

--- Udemy/MasterJava/HolaMundo/src/HolaMundo.java
+++ Udemy/MasterJava/HolaMundo/src/HolaMundo.java
@@ -1,6 +1,20 @@
 public class HolaMundo {
     public static void main(String[] args){
-        System.out.println("Hola Mundo desde Java");
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+        var numero3 = "15";
     }
 }
 
