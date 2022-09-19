02eca8ce0c239befab5d1642bf78c6dce25e63b2
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-18 21:33:04 -0500

    Class 14: AppBar Transitions

--- Udemy/MaterialUI/arcdevelopment/src/components/ui/Header.js
+++ Udemy/MaterialUI/arcdevelopment/src/components/ui/Header.js
@@ -1,15 +1,41 @@
+import React from 'react';
 import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar"
+import Toolbar from "@mui/material/Toolbar";
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+
+function ElevationScroll(props) {
+    const { children} = props;
+    
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0
+    
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+  
 
 
 export default function Header(props){
     return (
+        <ElevationScroll>
         <AppBar position="fixed">
             <Toolbar>
                 Arc Development
             </Toolbar>
         </AppBar>
+        </ElevationScroll>
     )
 }
 
-//El componente AppBar viene por defecto con position="fixed"
+/**
+ *  Appbar viene por defecto con "position: fixed"
+ *  De MaterialUI en la documentación del componente AppBar busco
+ * scrolling y luego copio el código de ElevationScroll y pego.
+ * 
+ * 
+ */
