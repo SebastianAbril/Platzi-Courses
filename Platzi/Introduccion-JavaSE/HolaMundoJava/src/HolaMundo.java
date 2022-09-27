3d173538edec642eb2afd2863ca979862a3dc5a1
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-27 09:07:07 -0500

    Etapas de programacion en java

--- Platzi/Introduccion-JavaSE/HolaMundoJava/src/HolaMundo.java
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/HolaMundo.java
@@ -3,3 +3,12 @@ public class HolaMundo {
         System.out.println("Hola Mundo");
     }
 }
+/*
+
+1. Escribir nuestros archivos .java
+2. Compilar, cargar y verificar nustros archivos con javac (los IDEs nos permiten compilar con solo presionar un botón)
+3. Al compilar obtenemos archivos .class con código que nuestras computadoras pueden entender (Byte Code).
+4. La JVM (Java Virtual Machine) se encarga de ejecutar el código de forma que funcione en cualquier dispositivo o sistema operativo.
+
+Por lo tanto Java es compilado e interpretado
+ */
