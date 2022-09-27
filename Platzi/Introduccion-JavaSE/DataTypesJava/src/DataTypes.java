119d62d17b23199044eb3c332af5ad76bfb39828
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-27 15:37:54 -0500

    Tipos de datos númericos

---
+++ Platzi/Introduccion-JavaSE/DataTypesJava/src/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567890;
+        long nL = 12345678901L;
+
+        double nD = 123.456;
+        float nF = 123.456F;
+    }
+}
+
+/*
+Enteros:
+-byte (1 byte)
+-short (2 bytes)
+-int (4 bytes)
+-long (8 bytes)
+ */
+
+/*
+Punto Flotante o decimal
+-float (4 bytes)
+-double (8 bytes)
+ */
