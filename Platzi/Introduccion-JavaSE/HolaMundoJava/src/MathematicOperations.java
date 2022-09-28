21862018055f0b5794d47fe190ba60f469644056
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-28 07:36:14 -0500

    Operaciones matemáticas

---
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/MathematicOperations.java
@@ -0,0 +1,35 @@
+public class MathematicOperations {
+    public static void main(String[] args) {
+        double x = 2.1;
+        double y = 3;
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        // Devuelve un entero hacia arriba
+        System.out.println(Math.ceil(x));
+
+        // Devuelve un entero hacia abajo
+        System.out.println(Math.floor(x));
+
+        //Devuelve ujn número elevado a otro
+        System.out.println(Math.pow(x,y));
+        //Devuelve el numero mayor
+        System.out.println(Math.max(y,x));
+
+        //Devuelve la raíz cuadrada
+        System.out.println(Math.sqrt(y));
+
+        //Area de un circulo
+        // pi * r2
+        System.out.println(Math.PI * Math.pow(y,2));
+
+        //Area de una esfera
+        // 4*PI*r2
+        System.out.println(4 * Math.PI * Math.pow(y,2));
+
+        //Volumen de una esfera
+        // 4/3 * PI * r3
+        System.out.println((4/3) * Math.PI * Math.pow(y,3));
+    }
+}
