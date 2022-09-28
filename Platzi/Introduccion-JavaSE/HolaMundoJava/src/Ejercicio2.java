2d831bcc4a69001487505c96cdb30897d91a5ffa
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-28 14:11:23 -0500

    Sentencia if

--- Platzi/Introduccion-JavaSE/HolaMundoJava/src/Ejercicio2.java
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/Ejercicio2.java
@@ -30,3 +30,4 @@ public class Ejercicio2 {
         System.out.println(number3Result);
     }
 }
+
---
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/IfStatements.java
@@ -0,0 +1,13 @@
+public class IfStatements {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled = true;
+        int fileSended = 3;
+
+        if (isBluetoothEnabled){
+            // Send file
+            fileSended++;
+            System.out.println("Archivo Enviado");
+            
+        }
+    }
+}
