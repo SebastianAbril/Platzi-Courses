935edf36fa8bced050c012ffb24c187b4ac2d091
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-28 10:20:51 -0500

    Clase 19: casting entre tipos de datos

--- Platzi/Introduccion-JavaSE/HolaMundoJava/src/Casting.java
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/Casting.java
@@ -17,5 +17,16 @@ public class Casting {
 
         System.out.println((double) a/b);
 
+        double c = (double)a/b;
+        System.out.println(c);
+
+        char n = '1';
+        int nI = n;
+        System.out.println(nI);
+
+        short nS = (short) n;
+        System.out.println(nS);
+
+
     }
 }
