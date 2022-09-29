714506bc1dce56800626f7d315b17995e6315de5
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-09-28 22:33:10 -0500

    Implementa funciones en Java, clase #27

---
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/Funciones.java
@@ -0,0 +1,33 @@
+public class Funciones {
+    public static void main(String[] args) {
+        double y = 3;
+        double area = circleArea(y);
+        System.out.println(area);
+        sphereArea(y);
+        sphereVolumen(y);
+
+        System.out.println("PESOS A DOLARES: " + convertToDolar(200, "MXN"));
+        System.out.println("PESOS A DOLARES: " + convertToDolar(1000, "COP"));
+    }
+    public static double circleArea(double r){
+        return Math.PI * Math.pow(r,2);
+    }
+    public static double sphereArea(double r){
+        return 4* Math.PI * Math.pow(r,2);
+    }
+    public static double sphereVolumen(double r){
+        return (4/3) * Math.PI * Math.pow(r,3);
+    }
+    public static double convertToDolar(double quantity, String currency){
+       // MXN COP
+        switch (currency){
+            case "MXN":
+                quantity = quantity * 0.052;
+                break;
+            case "COP":
+                quantity = quantity * 0.00031;
+                break;
+        }
+        return quantity;
+    }
+}
