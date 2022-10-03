85990de6cb2fd7389df7c12c1eeb6317eb882eff
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-10-03 16:38:44 -0500

    Indices y busqueda de elementos en Arrays

--- Platzi/Introduccion-JavaSE/HolaMundoJava/src/Arrays.java
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/Arrays.java
@@ -8,9 +8,41 @@ public class Arrays {
         int[][][] numbers = new int[2][2][2];
         int[][][][] numbers4 = new int[2][2][2][2];
         // Convencion de nombres de Arrays en plural
+
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);
+
+        cities[0][0] = "Colombia";
+        cities[0][1] = "Medellin";
+        cities[1][0] = "Colombia";
+        cities[1][1] = "Bogotá";
+        cities[2][0] = "Mexico";
+        cities[2][1] = "Guadalajara";
+        cities[3[0] = "México";
+        cities[3][1] = "CDMX";
+
+        System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);
+
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1] = "Monkey";
     }
 }
 
+Chango[1][0][0][1]
 
 /*
  * +------------------------------+
