69a27d1e3c9ea6d66f8bb2ede5c268379c84ddf0
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-10-03 17:19:14 -0500

    Ciclos For anidados

--- Platzi/Introduccion-JavaSE/HolaMundoJava/src/Arrays.java
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/Arrays.java
@@ -13,11 +13,27 @@ public class Arrays {
         androidVersions[1] = "Banana Bread";
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
+                                //17
+        for(int i=0; i < androidVersions.length; i++){
 
+            System.out.println(androidVersions[i]);
+        }
+
+        for(String androidVersion: androidVersions){
+            System.out.println(androidVersion);
+        }
+
+        for(int i=0; i < days.length; i++){
+
+            System.out.println(days[i]);
+        }
+/*
         System.out.println(androidVersions[0]);
         System.out.println(androidVersions[1]);
         System.out.println(androidVersions[2]);
         System.out.println(androidVersions[3]);
+
+ */
 
         cities[0][0] = "Colombia";
         cities[0][1] = "Medellin";
@@ -25,7 +41,7 @@ public class Arrays {
         cities[1][1] = "Bogotá";
         cities[2][0] = "Mexico";
         cities[2][1] = "Guadalajara";
-        cities[3[0] = "México";
+        cities[3][0] = "México";
         cities[3][1] = "CDMX";
 
         System.out.println(cities[0][0]);
@@ -37,12 +53,37 @@ public class Arrays {
         System.out.println(cities[3][0]);
         System.out.println(cities[3][1]);
 
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+
+        for(String[] pair:cities){
+            for(String name:pair){
+                System.out.println(name);
+            }
+        }
+
         String[][][][] animals = new String[2][3][2][2];
         animals[1][0][0][1] = "Monkey";
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 0; j++) {
+                for (int k = 0; k <= 0; k++) {
+                    for (int l = 0; l <= 1; l++) {
+                        System.out.println(animals[i][j][k][l]);
+
+                    }
+                }
+
+            }
+
+        }
     }
 }
 
-Chango[1][0][0][1]
+
 
 /*
  * +------------------------------+
