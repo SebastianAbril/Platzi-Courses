0a739999916619a716104cc10cd8bff2c1125b26
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-10-03 13:08:27 -0500

    Declarando Arrays

---
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/Arrays.java
@@ -0,0 +1,24 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2];
+
+        int[][][] numbers = new int[2][2][2];
+        int[][][][] numbers4 = new int[2][2][2][2];
+        // Convencion de nombres de Arrays en plural
+    }
+}
+
+
+/*
+ * +------------------------------+
+ * |  Country    |  City          |
+ * --------------------------------
+ * | México      | CDMX           |
+ * | México      | Guadalajara    |
+ * | Colombia    | Bogotá         |
+ * | Colombia    | Medellín       |
+ * +------------------------------+
+ * */
--- Platzi/Introduccion-JavaSE/HolaMundoJava/src/ForLoop.java
+++ Platzi/Introduccion-JavaSE/HolaMundoJava/src/ForLoop.java
@@ -14,7 +14,22 @@ public class ForLoop {
         return isTurnOnLight;
     }
 }
-
+ /**
+  * Las opciones que hay para detener ciclos y controlar el fujo son:
+  * 1. Break:
+  * - En un Swtich: en esta situación break hace que el flujo del switch no continue ejecutandose a la sigueinte comparación, esto con el objetivo de que
+  * solo se cumpla en una condición.
+  * - Para salir de un bucle: break es capaz de detener el flujo en el código, en este caso detendremos el ciclo como tal terminandolo y haciendo
+  * que saltemos a la siguiente instrucción despues del ciclo.
+  *
+  * 2. Continue: Continue en cierto modo también nos va a serv ir para detener un ciclo pero en lugar de terminarlo como en el caso de break,
+  * este volverá directo a la condición.
+  *
+  *3. Return: aunque en algunos lenguajes esta sentencia sirve como un tipo goto, dónde se rompe el flujo del programa
+  * la mejor forma de usarlo en Java es en Funciones, cuando lo usamos aquísiempre viene acompañado de un valor, el cual indica
+  * el dato que se estará devolviendo en la función.
+  *
+  */
 
 
 
