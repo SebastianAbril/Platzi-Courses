325da70119560feaea60a8717e7653c9bd1b9983
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-10-04 12:29:08 -0500

    Variables y Métodos estáticos

--- Platzi/Introduccion-JavaSE/MyMedicalAppointments/src/Main.java
+++ Platzi/Introduccion-JavaSE/MyMedicalAppointments/src/Main.java
@@ -7,3 +7,10 @@ public class Main {
         myDoctor.showName();
     }
 }
+
+/**
+ * Metodos static
+ * ¿Que es Static? se utiliza para definir métodos o variables de ese tipo estático. Utilizamos Static para llamar una variable sin utilizar un objeto. Se invoca en una clase que no tiene instancias de la clase.
+ *
+ * ¿Que nivel de Scope tiene una variable de tipo Static? Una variable de ese tipo, mantiene su ciclo de vida en todo el programa mientras este corriendo el programa.
+ */
