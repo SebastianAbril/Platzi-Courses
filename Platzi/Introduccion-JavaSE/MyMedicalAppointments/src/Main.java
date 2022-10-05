ece267f51ca307f679b2e3bf5c5dda21803b20db
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-10-05 08:32:44 -0500

    Clase 8: creando elementos estaticos

--- Platzi/Introduccion-JavaSE/MyMedicalAppointments/src/Main.java
+++ Platzi/Introduccion-JavaSE/MyMedicalAppointments/src/Main.java
@@ -1,11 +1,26 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Sebastian Abril"); // Metodo Constructor
+        Doctor myDoctor = new Doctor(); // Metodo Constructor
         myDoctor.name = "Irina";
         myDoctor.speciality = "Pediatra";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor.id++;
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
+
     }
+
+
 }
 
 /**
