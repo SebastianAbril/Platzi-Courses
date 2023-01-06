b09aab9ed973b534d9d0501cf1456a1a95bac592
Juan Sebastian Abril <sebastianabril6@gmail.com>
2023-01-05 19:26:00 -0500

    String interpolation, clase 6

--- Platzi/Fundamentos_ANGULAR/my-store/src/app/app.component.ts
+++ Platzi/Fundamentos_ANGULAR/my-store/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  name = 'Sebastian'
+  lastName = 'Abril'
+  age = 30;
+  foto = 'https://source.unsplash.com/random'
+
 }
+// LOS ATRIBUTOS TIENEN QUE SER PUBLICOS, TS POR DEFECTO LOS PONE PUBLICOS
