efe95a9b65d37354aaf935f8af0c86ec247e4487
Juan Sebastian Abril <sebastianabril6@gmail.com>
2023-03-17 12:00:21 -0500

    Nivelacion: clase 18:primeros pasos

---
+++ Udemy/Springboot/my-first-app/src/com/sebas/Notas.java
@@ -0,0 +1,35 @@
+package com.sebas;
+
+public class Notas {
+}
+
+/*
+* ¿Qué es un objeto?
+* Es una instancia de la clase, representa cosas. Pueden ser simples o complejos, reales o imaginarios.
+*
+* Por ejemplo: podemos instanciar un objeto de marca Sony modelo Bravia y otro objeto marca Samsung y modelo
+* Crystal UHD.
+*
+* // Primer objeto de la clase Televisor
+* Televisor sony = new Televisor(); // se instancia a través de la palabra reservada new
+* sony.nombreDelMetodo();
+*
+* //Segundo objeto de la clase Televisor
+* Televisor samsung = new Televisor();
+* samsumg.nombreDelMetodo();
+*
+* */
+
+/*
+* ¿QUE ES UNA CLASE?
+*
+* Es una definición de un objeto
+* Conjunto declarativo de atributos y metodos
+*
+* Por ejemplo: una clase televisor puede tener atributos tales como marca y modelo y métodos como encender, apagar y cambiar de canal.
+*
+* [Modificador de acceso] class NombreClase{
+*   // Declaración de atributos o campos
+*   // Declaración de métodos
+* }
+* */
