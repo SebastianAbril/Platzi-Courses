5b704060c351028243db0375b227b38c1b44e560
Juan Sebastian Abril <sebastianabril6@gmail.com>
2023-03-24 00:46:22 -0500

    Curso de Maven: clase #22: primeros pasos

--- Udemy/Springboot/my-first-app/src/com/sebas/Notas.java
+++ Udemy/Springboot/my-first-app/src/com/sebas/Notas.java
@@ -33,3 +33,33 @@ public class Notas {
 *   // Declaración de métodos
 * }
 * */
+
+/*
+*    ¿QUÉ ES MAVEN?
+*     es un Bill Tools y ademas es un Project Management:
+*    - Gestor de dependencias
+*    - Estructura standard de proyecto (Archetypes)
+*    - Plugins orientados a proyectos
+*    - Soporte para fases del proyecto (Lifecycle)
+*    - Documentación de proyecto
+*    - Soporte para gestión avanzada de proyecto
+*
+*     ¿ COMO SE INSTALA?
+*       Ya viene incluido en le IDE de IntelliJ
+*
+*      CICLO DE VIDA MAVEN
+*      Es el concepto fundamental para trabajar con mavenm.
+*      - Proceso por el cual podremos construir y distrbuir una artefacto, proyecto.
+*      - Esta compuesto por varias fases (comandos):
+*
+*           - 1.Clean (mvn clean): limpia el espacio de trabajo que utiliza Maven, por lo general carpeta target, del directorio raíz de nuestro proyecto .
+*           - 2.Validate (mvn clean): Solamente valida que el proyecto dispone de toda la información necesaria para ser procesado. Muy rara vez suele utilizarse.
+*           - 3.Compile (mvn compile): Se encarga de compilar nuestro proyecto, es decir archivos.java y generar en carpetas de destinos los compilados.
+*           - 4.Test (mvn test): AL finalizar la compilación, se ejecutan las pruebas unitarias que se hah construido. Con el fin de validar que nuestro código es correcto.
+*           - 5.Package (mvn package): Se encarga de empaquetar nuestros fuentes en un formato estándar de Java, permitiendo su ejecución o despliegue en servidores.
+*           - Verify (mvn verify): Se encarga de ejecutar los test de integración para confirmar que todo funciona correctamente y que la calidad es la correcta.
+*           - Install (mvn install): Se encarga de desplegar el artefacto en el repositorio local con su correspondiente versión.
+*           - Site (mvn site): Se encarga de generar el sitio de documentación de nuestro proyecto.
+*           - Deploy (mvn deploy): Similar a Install, con la diferencia que el artefacto se aloja en un repositorio remoto.
+*
+* */
---
+++ Udemy/Springboot/my-first-maven-app/src/main/java/com/mycompanyname/maven/HolaMundo.java
@@ -0,0 +1,7 @@
+package com.mycompanyname.maven;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo.");
+    }
+}
