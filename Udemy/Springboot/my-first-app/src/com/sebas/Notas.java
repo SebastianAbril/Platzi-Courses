256e56a6ff28b29f310651a83e353c101f46c56b
Juan Sebastian Abril <sebastianabril6@gmail.com>
2023-04-12 14:02:14 -0500

    Clase 28:Hibernate, primera entidad

--- Udemy/Springboot/my-first-app/src/com/sebas/Notas.java
+++ Udemy/Springboot/my-first-app/src/com/sebas/Notas.java
@@ -63,3 +63,38 @@ public class Notas {
 *           - Deploy (mvn deploy): Similar a Install, con la diferencia que el artefacto se aloja en un repositorio remoto.
 *
 * */
+
+/*
+*   JAVA PERSISTENCE API (JPA), es un estándar para la persistencia de objetos en base de datos relacionales.
+* Nos permite realizar operaciones básicas (CRUD), sin la necesidad de escribir cada sentencia SQL.
+*
+* CONCEPTOS BÁSICOS:
+*
+* - Persistence.xml: archivo que contiene la información necesaria para conectarnos a nuestra base de datos y define el conjunto de entidades a gestionar.
+* - EntityManagerFactory: es conocido como PersistenceUnit o Unidad de Persistencia. Es único y es con el que gestionamos todas las entidades.
+*
+* - EntityManager: es creado por el EntityManagerFactory y gestiona un conjunto de entidades u objetos. El EntityManager es el encargado de guardar, eliminar los ojetos de la base de datos.
+* - PersistenceContext: hace referencia a los objetos que han sido manipulados por el EntityManager y se encuentran bajo su control, una vez que se hayan invocado a los metodos persist,merge,ect del EntityManager.
+*
+*
+*
+*  ¿QUE ES ORM?
+*
+*   Object Relational Mapping, lo que nos permite es poder mapear nuestros objetos a entidades en nuestra base de datos relacional.
+*   En donde las clases representan las tablas y los atributos de estas clases representan a los campos de las tablas.
+*
+* ¿ QUÉ ES UN FRAMEWORK?
+*
+* Es una construcción de Software que nos provee una solución para una problemática determinada, para este caso el manejo con la base de datos.
+* Los framework nos facilita enormemente el proceso de desarrollo de nuestras aplicaciones ya que no debemos de poner foco en como resolver ciertas problemáticas que se nos presenta.
+*
+* ¿ QUÉ ES HIBERNATE?
+*
+* Es un framework de persistencia y acceso de datos que se encuentren alojados en una base de datos. Se encuentra basado en el estándar de Java, conocido como JPA.
+*
+* Ventajas de usar Hibernate:
+* - No tenemos que preocuparnos por el control de datos, por ejemplo valores nulos.
+* - No tenemos que preocuparnos cual es el motor de base de datos a utilizar, nos abstrae del mismo.
+* - No tenemos que estar preocupándonos si modificamos alguna entidad, aplica los cambios solo.
+* - Nos evita el mapeo de datos a través del setString, setInt,etc que utilizamos al usar el API de JDBC.
+* */
