bae95050dfc55b308885e1bf2998adb197316170
SebastianAbril6 <sebastianabril6@gmail.com>
2023-05-28 20:07:58 -0500

    course de fundamentos de typescript: hasta la clase 9

---
+++ Platzi/fundamentos-typescript/ts-project/src/04-numbers.js
@@ -0,0 +1,16 @@
+(function () {
+    var productPrice = 100;
+    productPrice = 12;
+    console.log('productPrice', productPrice);
+    var customerAge = 28;
+    //customerAge = customerAge + "1"; //en js se supone que daria 281
+    customerAge = customerAge + 1;
+    console.log('customerAge', customerAge);
+    var productInStock;
+    console.log('productInStock', productInStock);
+    if (productInStock > 10) {
+        console.log('is greater');
+    }
+    var discount = parseInt('1234');
+    console.log('discount', discount);
+})();
---
+++ Platzi/fundamentos-typescript/ts-project/src/04-numbers.ts
@@ -0,0 +1,31 @@
+(() => {
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('productPrice', productPrice);
+
+  let customerAge: number = 28;
+  //customerAge = customerAge + "1"; //en js se supone que daria 281
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge);
+
+  let productInStock: number;
+  console.log('productInStock', productInStock);
+  if (productInStock > 10) {
+    console.log('is greater');
+  }
+
+  let discount = parseInt('1234');
+  console.log('discount', discount);
+  if (discount <= 200) {
+    console.log('apply');
+  } else {
+    console.log('not apply');
+  }
+
+  let hex = 0xff;
+  console.log('hex', hex);
+  let bin = 0b101011;
+  console.log('bin', bin);
+
+  const myNumber: number = 10;
+})();
